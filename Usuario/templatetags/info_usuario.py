88751f3a22d832ba2076f3f437f8621412516c94
Carla Fernandes <carla.fernandes@ufrn.br>
2025-06-26 13:02:31 -0300

    Ajustar senha close #37

--- Usuario/templatetags/info_usuario.py
+++ Usuario/templatetags/info_usuario.py
@@ -7,3 +7,11 @@ register = template.Library()
 def usuario_avatar(username):
     usuario_logado = Usuario.objects.filter(username=username)[0]
     return usuario_logado.avatar.url
+
+@register.filter
+def iniciais_nome_completo(user):
+    if not user.first_name and not user.last_name:
+        return ""
+    primeira = user.first_name.strip()[0] if user.first_name else ""
+    ultima = user.last_name.strip()[0] if user.last_name else ""
+    return f"{primeira.upper()}{ultima.upper()}"
